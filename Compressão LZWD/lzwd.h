10b0856c2ddf8ccf5eac0597e5b5eee488625427
Rui Cunha <101945306+ruipCunha0@users.noreply.github.com>
2025-05-14 15:21:48 +0100

    Projeto final

---
+++ Compressão LZWD/lzwd.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <string.h>
+
+#define MAX_DICT_SIZE 65536
+#define MAX_BLOCK_SIZE 65536
+
+typedef unsigned char byte_t; // 8-bit byte
